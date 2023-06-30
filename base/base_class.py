cfe379202d98664ccf75ab032d55a33d57b58f74
Dmitriy <dimokse@gmail.com>
2023-06-30 09:23:47 +0300

    Добавлен run_test.yml

--- base/base_class.py
+++ base/base_class.py
@@ -5,6 +5,7 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
+from  selenium.webdriver.chrome.options import Options
 
 """ Класс BasePage служит в качестве базового класса, который содержит 
 общие методы и функциональность.
