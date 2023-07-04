5f0c331d630066ef0dca6c726dfa98c9b5d4c859
Dmitriy <dimokse@gmail.com>
2023-07-04 15:19:33 +0300

    add вызов метода проверки корзины

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
+from pages.card_pages import CardPage
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
 
@@ -10,21 +11,20 @@ options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 # options.add_argument('--headless')  # Безголовый режим(без запуска браузера)
 
-
-# def browser():
-#     driver = webdriver.Chrome()
-#     driver.maximize_window()
-#     driver.get("https://sad-i-ogorod.ru/")
-
 # @pytest.fixture()
 def test_login(set_group):
     service = Service('H:\\Python.Selenium_lesson-1\\Python_selenium\\chromedriver.exe')
     driver = webdriver.Chrome(service=service, options=options)
     print("Старт теста")
     login_page = LoginPage(driver)
-    login_page.authorization()
+    login_page.authorization()  # Авторизация на странице.
 
     main_page = MainPage(driver)
-    main_page.go_novinki()
-    main_page.go_to_card()
+    main_page.go_novinki()  # Переход на страницу "Новинки"
+    main_page.go_to_card()  # Переход на станицу "Корзина"
+
+    card_page = CardPage(driver)
+    card_page.check_card()  # Проверка странице "Корзина"
+
+
 
