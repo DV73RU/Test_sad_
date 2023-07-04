0cd99772d08cd8f741412537524a757e0cef44d4
Dmitriy <dimokse@gmail.com>
2023-07-04 15:18:56 +0300

    fix

--- pages/novinki_page.py
+++ pages/novinki_page.py
@@ -15,9 +15,3 @@ class NovinkiPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
-
-# novinki_page = NovinkiPage(driver)
-# novinki_page.go_to_page()
-#
-# # Добавление товаров в корзину
-# novinki_page.add_products_to_cart()
