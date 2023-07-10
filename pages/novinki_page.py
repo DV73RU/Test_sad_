910e2732fe584a13b98e03afdecdad77154dbc95
Dmitriy <dimokse@gmail.com>
2023-07-10 14:44:44 +0300

    add Метод проверки странице Новинки

--- pages/novinki_page.py
+++ pages/novinki_page.py
@@ -1,17 +1,24 @@
 """
 Класс странице Новинки
 """
-from selenium import webdriver
-
 from base.base_class import BasePage
 
 
-driver = webdriver.Chrome()
-
-
-class NovinkiPage(BasePage):
+class NewsPage(BasePage):
     url = 'https://sad-i-ogorod.ru/catalog/novinki.html'
 
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
+
+    # Локаторы элементов страницы.
+    header_news = "//h1[@style='margin-bottom: 0px;']"
+    info_wrapper = "//a[@class='prod-name js-prod-link-list']"  # Локатор карточки товара на старание продуктов.
+    """
+    Метод проверки заголовка странице
+    """
+
+    def check_news(self):
+        self.check_page_header(self.header_news, "Новинки")  # Проверяем значение заголовка странице Новинки.
+        self.parse_product(self.info_wrapper)  # Парсим товары на странице
+        self.check_cart()  # Проверяем состояние иконнки корзины
