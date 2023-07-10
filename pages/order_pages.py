aeb370201d9f7aa85e3e2549c96fbf99a2a53c9b
Dmitriy <dimokse@gmail.com>
2023-07-10 14:45:18 +0300

    add Класс странице Ордер

---
+++ pages/order_pages.py
@@ -0,0 +1,35 @@
+"""
+Класс странице Ордер
+"""
+from base.base_class import BasePage
+
+
+class OrderPage(BasePage):
+    url = 'https://sad-i-ogorod.ru/cart/order/'
+
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.driver = driver
+
+    header_order = "//div[@class = 'h2 pt_5 mb_15']"    # Заголовок странице
+    input_surname = "//input[@name = 'surname']"  # Фамилия
+    input_name = "//input[@name='name']"  # Имя
+    input_father = "//input[@name = 'father']"     # Отчество
+    input_phone = "//input[@name = 'phone']"    # Номер телефона
+    input_email = "//input[@name = 'email']"    # email
+    button_next = "//div[@class = 'btn js-next']"   # Кнопка Далее
+    # // TODO Проверка заполненности полей
+
+    """
+    Метод проверки заголовка странице Ордер
+    """
+
+    def check_order(self):
+        self.check_page_header(self.header_order, "Оформление заказа")  # Проверяем значение заголовка странице Корзина.
+
+    def check_input(self): # Возвращаем текст введённый в поля ввода.
+        self.get_text(self.input_name)  # Что введено в поле имя
+        self.get_text(self.input_surname)
+        self.get_text(self.input_father)
+        self.get_text(self.input_email)
+        self.get_text(self.input_phone)
