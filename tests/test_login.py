b21d203fe68b58b45213419b953a7df3d231c550
Dmitriy <dimokse@gmail.com>
2023-07-11 16:11:50 +0300

    fic вызовы методов

--- tests/test_login.py
+++ tests/test_login.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from pages.card_pages import CardPage
 from pages.login_page import LoginPage
 from pages.main_page import MainPage
+from pages.novinki_page import NewsPage
 from pages.order_pages import OrderPage
 
 options = webdriver.ChromeOptions()
@@ -19,17 +20,20 @@ def test_login(set_group):
     service = Service('H:\\Python.Selenium_lesson-1\\Python_selenium\\chromedriver.exe')
     driver = webdriver.Chrome(service=service, options=options)
     print("Старт теста")
-    login_page = LoginPage(driver)
-    login_page.authorization()  # Авторизация на странице.
+    # login_page = LoginPage(driver)
+    # login_page.authorization()  # Авторизация на странице.
 
     main_page = MainPage(driver)
     main_page.go_news_page()  # Переход на страницу "Новинки"
-    main_page.add_to_cart()  # Добавление в корзину
-    main_page.go_to_card()  # Переход на станицу "Корзина"
 
-    card_page = CardPage(driver)
-    card_page.check_card()  # Проверка странице "Корзина"
-    card_page.go_to_order()     # Переход на страницу Оформление заказа
+    news_pages = NewsPage(driver)
+    # news_pages.check_page_header()  # Проверяем заголовок странице Новинки
+    news_pages.check_news()  # Чекаем товары на странице Новинки.
+    # news_pages.go_
 
-    order_page = OrderPage(driver)
-    order_page.check_order()    # Проверка станице Оформление заказа
+    # card_page = CardPage(driver)
+    # card_page.check_card()  # Проверка странице "Корзина"
+    # card_page.go_to_order()     # Переход на страницу Оформление заказа
+
+    # order_page = OrderPage(driver)
+    # order_page.check_order()    # Проверка станице Оформление заказа
