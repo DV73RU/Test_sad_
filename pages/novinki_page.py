1f96b64b1c230d0273551feb00d93de77b8c75ad
Dmitriy <dimokse@gmail.com>
2023-07-11 16:10:55 +0300

    add self.url, self.wait, разбиты действия

--- pages/novinki_page.py
+++ pages/novinki_page.py
@@ -1,6 +1,8 @@
 """
 Класс странице Новинки
 """
+from selenium.webdriver.support.wait import WebDriverWait
+
 from base.base_class import BasePage
 
 
@@ -10,15 +12,21 @@ class NewsPage(BasePage):
     def __init__(self, driver):
         super().__init__(driver)
         self.driver = driver
+        self.url = 'https://sad-i-ogorod.ru/'
+        wait_timeout = 10  # Увеличьте время ожидания, если это необходимо
+        self.wait = WebDriverWait(self.driver, wait_timeout)
 
     # Локаторы элементов страницы.
     header_news = "//h1[@style='margin-bottom: 0px;']"
     info_wrapper = "//a[@class='prod-name js-prod-link-list']"  # Локатор карточки товара на старание продуктов.
+
+    # Actions
     """
     Метод проверки заголовка странице
     """
 
     def check_news(self):
         self.check_page_header(self.header_news, "Новинки")  # Проверяем значение заголовка странице Новинки.
+
+    def parse_news(self):
         self.parse_product(self.info_wrapper)  # Парсим товары на странице
-        self.check_cart()  # Проверяем состояние иконнки корзины
