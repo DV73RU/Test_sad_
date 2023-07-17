a050cef65d07955d115ed6c2f11e3089b59279f3
Dmitriy <dimokse@gmail.com>
2023-07-17 18:18:26 +0300

    fix

--- pages/card_pages.py
+++ pages/card_pages.py
@@ -24,6 +24,7 @@ class CardPage(BasePage):
     total_price_card = "//div[@class='bask-page__parcelTotal']/span[@class='bask-page__parcelTotal-price']"  # Итоговая сумма в корзине
     button_order = "//button[@class = 'bask-page__orderTotal-btn']"  # Кнопка "Оформить заказ"
     product_list = "//tr[contains(@class, 'bask-item')]" # Локатор товаров козины
+
     # // TODO Перенести все локаторы корзины.
 
     # Actions
@@ -35,7 +36,7 @@ class CardPage(BasePage):
         self.check_page_header(self.header_card, "Корзина")  # Проверяем значение заголовка странице Корзина.
 
     def parse_card(self):
-        self.parse_products_card(self.product_list, print_products= True)  # Парсим товары на странице Корзины
+        self.parse_products_card(self.product_list, print_products=True)  # Парсим товары на странице Корзины
 
     def click_button_order2(self):  # Клик на кнопку Оформить ордер если мешает pop-ap
         self.click_element(self.button_order)
@@ -47,5 +48,6 @@ class CardPage(BasePage):
     def go_to_order(self):
         self.driver.get(self.url)
         # self.driver.maximize_window()
+        # self.scroll_pages_to_element(self.button_order)
         self.click_button_order2()  # Кликаем на кнопку Ордер.
         self.assert_url_2('https://sad-i-ogorod.ru/cart/order/')  # Проверка ожидаемой url
