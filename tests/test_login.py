b2bcc3bfa3a97c83922bd5ae29f691f39920f82f
Dmitriy <dimokse@gmail.com>
2023-07-21 17:55:56 +0300

    fix Вызов экземпляр класса

--- tests/test_login.py
+++ tests/test_login.py
@@ -19,6 +19,7 @@ options.add_experimental_option("detach", True)
 options.add_argument("start-maximized")
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
 options.add_experimental_option('useAutomationExtension', False)
+options.add_argument('--headless')	# Без окна браузера.
 exec_path = os.path.join(os.getcwd(), 'driver', 'chromedriver.exe') if system() == "Windows" else \
     os.path.join(os.getcwd(), 'driver', 'chromedriver')
 driver = webdriver.Chrome(options=options, service=Service(log_path=os.devnull, executable_path=exec_path))
@@ -39,51 +40,28 @@ stealth(driver=driver,
 # options.add_argument('--headless')  # Безголовый режим(без запуска браузера)
 
 # @pytest.fixture()
-# def test_login(set_group):
-#     service = Service('H:\\Python.Selenium_lesson-1\\Python_selenium\\chromedriver.exe')
-#     # driver = webdriver.Chrome(options=options, service=Service(log_path=os.devnull, executable_path=exec_path))
-#     # driver = webdriver.Chrome(service=service, options=options)
-#     print("Старт теста")
-#     # login_page = LoginPage(driver)
-#     # login_page.authorization()  # Авторизация на странице.
+
+# def test_seeds_pages2(set_group):
+#     print("Старт теста странице 'Семена'")
+#     main_page = MainPage(driver)
+#     main_page.go_seeds_pages()
+#     seed_page = SeedsPage(driver)
+#     seed_page.check_seeds()
+#     seed_page.parse_seeds()
+#     seed_page.add_to_cart(max_cart_total=1500)
 #
 #     main_page = MainPage(driver)
-#     main_page.go_news_page()  # Переход на страницу "Новинки"
-#
-#     news_pages = NewsPage(driver)
-#     # news_pages.check_page_header()  # Проверяем заголовок странице Новинки
-#     news_pages.check_news()  # Чекаем товары на странице Новинки.
-#     news_pages.parse_news()
-#     news_pages.add_to_cart()
 #     main_page.go_to_card()
 #
 #     card_page = CardPage(driver)
+#     card_page.check_order()
 #     card_page.parse_card()
-#
-#     card_page.go_to_order()  # Переход на страницу Оформление заказа
-#
-#     order_page = OrderPage(driver)
-#     order_page.check_order()  # Проверка станице Оформление заказа
-#
-#
-# def test_seeds_pages(set_group):
-#
-#     print("Страт теста 'Семена'")
-#     main_page = MainPage(driver)
-#     main_page.go_seeds_pages()
+#     card_page.go_to_order()
+#     card_page.check_card()
 
 
-def test_seeds_pages2(set_group):
-    print("Старт теста странице 'Семена'")
-    seed_page = SeedsPage(driver)
-    seed_page.go_url_pages()
-    seed_page.check_seeds()
-    seed_page.parse_seeds()
-    seed_page.add_to_cart(max_cart_total=1500)
+def test_authorization(set_group):
+    print("Старт теста Авторизации")
+    login_page = LoginPage(driver)
+    login_page.authorization()
 
-    main_page = MainPage(driver)
-    main_page.go_to_card()
-
-    card_page = CardPage(driver)
-    card_page.parse_card()
-    card_page.go_to_order()
