cef209692abe3049ce8ce881efeea849597cd9d3
Dmitriy <dimokse@gmail.com>
2023-07-26 09:21:14 +0300

    add @pytest.fixture

--- conftest.py
+++ conftest.py
@@ -1,7 +1,13 @@
 import pytest
+import os
+from platform import system
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium_stealth import stealth
 
 
-@pytest.fixture()
+@pytest.fixture(scope="function")
 def set_up():
     print("Start test")
     yield
@@ -9,7 +15,30 @@ def set_up():
 
 
 @pytest.fixture(scope="module")
-def set_group():
-    print("Enter system")
-    yield
-    print("Exit system")
+def driver():
+    """Инициализация драйвера браузера перед запуском тестов."""
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("detach", True)
+    options.add_argument("start-maximized")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_experimental_option('useAutomationExtension', False)
+    exec_path = os.path.join(os.getcwd(), 'driver', 'chromedriver.exe') if system() == "Windows" else \
+        os.path.join(os.getcwd(), 'driver', 'chromedriver')
+    driver = webdriver.Chrome(options=options, service=Service(log_path=os.devnull, executable_path=exec_path))
+
+    stealth(driver=driver,
+            user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                       'Chrome/83.0.4103.53 Safari/537.36',
+            languages=["ru-RU", "ru"],
+            vendor="Google Inc.",
+            platform="Win32",
+            webgl_vendor="Intel Inc.",
+            renderer="Intel Iris OpenGL Engine",
+            fix_hairline=True,
+            run_on_insecure_origins=True,
+            )
+
+    yield driver
+
+    # Закрытие браузера после выполнения всех тестов.
+    driver.quit()
