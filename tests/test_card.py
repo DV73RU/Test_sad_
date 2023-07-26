a5832bc0a1ea8669f2c9349d0f8733277c94e8b7
Dmitriy <dimokse@gmail.com>
2023-07-26 09:22:52 +0300

    del Методы обхода защиты от автоматизированного ПО

--- tests/test_card.py
+++ tests/test_card.py
@@ -9,30 +9,8 @@ from selenium_stealth import stealth
 from pages.main_page import MainPage
 from pages.seeds_page import SeedsPage
 
-"""Методы обхода защиты от автоматизированного ПО в браузере Chrome под управлением Selenium в Python"""
-options = webdriver.ChromeOptions()
-options.add_experimental_option("detach", True)
-options.add_argument("start-maximized")
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option('useAutomationExtension', False)
-exec_path = os.path.join(os.getcwd(), 'driver', 'chromedriver.exe') if system() == "Windows" else \
-    os.path.join(os.getcwd(), 'driver', 'chromedriver')
-driver = webdriver.Chrome(options=options, service=Service(log_path=os.devnull, executable_path=exec_path))
 
-stealth(driver=driver,
-        user_agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
-                   'Chrome/83.0.4103.53 Safari/537.36',
-        languages=["ru-RU", "ru"],
-        vendor="Google Inc.",
-        platform="Win32",
-        webgl_vendor="Intel Inc.",
-        renderer="Intel Iris OpenGL Engine",
-        fix_hairline=True,
-        run_on_insecure_origins=True,
-        )
-
-
-def test_add_to_card(set_group):
+def test_add_to_card(driver):
     print("Старт теста Добавление товаров в корзину")
 
     main_page = MainPage(driver)
@@ -41,4 +19,3 @@ def test_add_to_card(set_group):
     seed_page.check_seeds()
     seed_page.parse_seeds()
     seed_page.add_to_cart2(max_cart_total=1500)
-
