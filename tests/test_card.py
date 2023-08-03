3cb34536c467b72d166ef0a668d20d00e36c804c
Dmitriy <dimokse@gmail.com>
2023-08-03 18:13:47 +0300

    пересборка def test_add_to_card

--- tests/test_card.py
+++ tests/test_card.py
@@ -1,10 +1,5 @@
+# test_card.py
 import pytest
-import os
-from platform import system
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium_stealth import stealth
 
 from pages.card_pages import CardPage
 from pages.main_page import MainPage
@@ -13,17 +8,20 @@ from pages.order_pages_login import OrderPageLogin
 from pages.seeds_page import SeedsPage
 
 
-@pytest.mark.parametrize("max_cart_total", [700, 1000, 2200])
+@pytest.mark.parametrize("max_cart_total", [700, 1000, 200])
 def test_add_to_card(driver, max_cart_total):
     print(f"Старт теста Добавление товаров в корзину с максимальной суммой {max_cart_total}")
 
     main_page = MainPage(driver)
-    main_page.go_seeds_pages()
+    main_page.check_main_page()    # Проверка URL главной станице
+    main_page.go_seeds_pages()     # Переход на страницу "Семена" // TODO ДУБЛЬ ПРОВЕРОК URL
+
     seed_page = SeedsPage(driver)
-    seed_page.check_seeds()
-    seed_page.parse_seeds()
-    seed_page.add_to_card2(max_cart_total)
-    seed_page.click_to_card()
+    seed_page.check_page_seeds()    # Проверка странице "Семена"
+    seed_page.parse_seeds()    # Парсинг товаров на странице "Семена"
+    seed_page.add_to_card_in_seed()  # Добавление товаров в корзину
+    seed_page.click_to_card()   # Переход на страницу Корзина
+
     card_page = CardPage(driver)
     card_page.check_card_header("Корзина")
     card_page.check_ur_card()
