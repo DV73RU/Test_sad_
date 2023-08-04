7c3227a1cf39dd22afccb2dff7b3e5e12f95388a
Dmitriy <dimokse@gmail.com>
2023-08-04 16:41:54 +0300

    вызываем с параметром max_card_total

--- tests/test_card.py
+++ tests/test_card.py
@@ -8,7 +8,7 @@ from pages.order_pages_login import OrderPageLogin
 from pages.seeds_page import SeedsPage
 
 
-@pytest.mark.parametrize("max_cart_total", [700, 1000, 200])
+@pytest.mark.parametrize("max_cart_total", [800, 2000, 2500])
 def test_add_to_card(driver, max_cart_total):
     print(f"Старт теста Добавление товаров в корзину с максимальной суммой {max_cart_total}")
 
@@ -19,16 +19,16 @@ def test_add_to_card(driver, max_cart_total):
     seed_page = SeedsPage(driver)
     seed_page.check_page_seeds()    # Проверка странице "Семена"
     seed_page.parse_seeds()    # Парсинг товаров на странице "Семена"
-    seed_page.add_to_card_in_seed()  # Добавление товаров в корзину
-    seed_page.click_to_card()   # Переход на страницу Корзина
-
-    card_page = CardPage(driver)
-    card_page.check_card_header("Корзина")
-    card_page.check_ur_card()
-    card_page.parse_card()
-    card_page.close_banner()
-    card_page.go_to_order()
-    order_page = OrderPage(driver)
-    order_page.authorization()
-    order_page_login = OrderPageLogin(driver)
-    order_page_login.check_pages()
+    seed_page.add_to_card_in_seed(max_cart_total)  # Добавление товаров в корзину
+    # seed_page.click_to_card()   # Переход на страницу Корзина
+    #
+    # card_page = CardPage(driver)
+    # card_page.check_card_header("Корзина")
+    # card_page.check_ur_card()
+    # card_page.parse_card()
+    # card_page.close_banner()
+    # card_page.go_to_order()
+    # order_page = OrderPage(driver)
+    # order_page.authorization()
+    # order_page_login = OrderPageLogin(driver)
+    # order_page_login.check_pages()
