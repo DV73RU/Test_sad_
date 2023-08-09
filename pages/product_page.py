9941399b6bad77ee35c8b5729893423fe0d8560e
Dmitriy <dimokse@gmail.com>
2023-08-09 17:20:03 +0300

    del не рабочие импорты

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,11 +1,8 @@
-# product_page.py
-from selenium.webdriver.support.wait import WebDriverWait
-
+# product_page.py=
+"""
+Класс карточки товара на странице.
+"""
 from base.base_class import BasePage
-
-"""
-Клаcс карточки товара на странице.
-"""
 
 
 class ProductPage(BasePage):
@@ -16,12 +13,11 @@ class ProductPage(BasePage):
 
     # Локаторы карточки товара на странице
     info_wrapper = ".//ancestor::form[@class='info-wrapper add-bask-form-list ']"  # Локатор карточки товара
-    button_add_to_card = "//button[@class ='to-cart-btn elem-to_cart']"# Локатор Кнопки добавить в корзину
-    product_name = ".//a[@class='prod-name js-prod-link-list']"   # Локатор название товара
+    button_add_to_card = "//button[@class ='to-cart-btn elem-to_cart']"  # Локатор Кнопки добавить в корзину
+    product_name = ".//a[@class='prod-name js-prod-link-list']"  # Локатор название товара
     product_price = ".//div[@class='prod-price ']"  # Локатор цены товара
-    product_img = ""    # Локатор картинки товара
+    product_img = ""  # Локатор картинки товара
     favorite = ""  # Локатор кнопки "Добавить в избранное"
-    counter = ""    # Локатор спинбокс
+    counter = ""  # Локатор спинбокс
     control_minus = ""  # Локатор кнопки '-'
-    control_plus = ""   # Локатор кнопки '+'
-
+    control_plus = ""  # Локатор кнопки '+'
