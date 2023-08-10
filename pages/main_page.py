d95af1c6048da75aae0968859c906574131eda0a
Dmitriy <dimokse@gmail.com>
2023-08-10 11:44:17 +0300

    fix описание

--- pages/main_page.py
+++ pages/main_page.py
@@ -15,33 +15,29 @@ class MainPage(BasePage):
         wait_timeout = 10  # Увеличьте время ожидания, если это необходимо
         self.wait = WebDriverWait(self.driver, wait_timeout)
 
-    # Локаторы.
+    # +---------------------------------------------+
+    # | Локаторы элементов страницы главная         |
+    # +---------------------------------------------+
     main_header = "//div[@class = 'main_hits_title']"
     button_add_to_cart_locator = "//button[@class='to-cart-btn elem-to_cart']"
-    # product_price_locator = "//div[@class='prod-price ']"
-    # product_names = [...]  # Список названий продуктов
     button_card = "//span[@class = 'price']"  # Кнопка Корзина если в ней есть товары
     button_card_f = "//span[@class='no-product']"  # Кнопка Корзины если в ней нет товаров
-    # Количество товара в корзине.
-    # Сумма заказа.
     button_novelties1 = "/html/body/div[1]/div[1]/div[4]/div/div/div[1]/a/span"  # Кнопка Новинки.
     button_novelties = "//a[@href='/catalog/novinki.html' and span[text()='Новинки']]"  # Кнопка Новинки.
     button_add_card = "//button[@class ='to-cart-btn elem-to_cart']"  # Кнопка добавить в корзину
     agree_button = "//a[@class = 'cookie-msg__button']"  # Кнопка "Согласен" модального окна.
     button_seeds = "//a[@href='/catalog/semena.html' and span[text()='Семена']]"  # Кнопка Меню Семена
 
-    # Локаторы карточки товара на странице
+    # +---------------------------------------------+
+    # | Локаторы элементов карточки товара          |
+    # +---------------------------------------------+
     add_to_cart_locator = "//button[@class='to-cart-btn elem-to_cart']"  # Локатор Кнопки добавить в корзину
     product_name_locator = ".//a[@class='prod-name js-prod-link-list']"  # Локатор название товара
     product_price_locator = ".//div[@class='prod-price ']"  # Локатор цены товара
 
-    # Меню Плодовые
-    # Меню Декоративные
-    # Меню Луковичные
-    # Кнопка Лук, чеснок
-    # Кнопка Чай
-    # Меню Сопутка
-
+    # +--------------------------------------+
+    # |            Actions                   |
+    # +--------------------------------------+
     def go_to_url_main_pages(self, url):
         self.go_to_url(self.url)
 
@@ -59,12 +55,15 @@ class MainPage(BasePage):
 
     def click_butt_card(self):
         self.click_element(self.button_card)  # Клик на кнопку Корзина.
+    # +-----------------------------------------+
+    # |       Методы главной странице           |
+    # +-----------------------------------------+
 
     """
     Метод перехода на страницу Семена
     """
 
-    def go_seeds_pages(self):
+    def go_seeds_pages(self):  # Переход на страницу "Семена"
         self.click_butt_seeds()  # Кликаем на кнопку новинки.
         self.assert_url('https://sad-i-ogorod.ru/catalog/semena.html')  # Проверка ожидаемой url
 
@@ -72,11 +71,11 @@ class MainPage(BasePage):
     Метод перехода в корзину
     """
 
-    def go_to_card(self):
+    def go_to_card(self):  # Переход на страницу корзина
         self.click_butt_card()  # Кликаем на кнопку Корзина.
         self.assert_url('https://sad-i-ogorod.ru/cart/')  # Проверка ожидаемой url
 
-    def check_main_page(self):
+    def check_main_page(self):  # Проверка перехода на главную страницу
         self.go_to_url_main_pages(self.url)
         self.assert_url_main("https://sad-i-ogorod.ru/")
         self.check_main_header('Хиты продаж')
